91291a16ac93611057e86b6b381ac9298e84bca8
Manish <manish.sharma22221@gmail.com>
2023-09-09 22:52:08 +0530

    Product details page added

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Navigation from './customer/components/navigation/Navigation';
 import HomePage from './customer/components/pages/HomePage';
 import Footer from './customer/components/Footer/Footer';
+import ProductDetails from './customer/components/ProductDetails/ProductDetails';
 
 function App() {
   return (
     <div className="App">
       <Navigation></Navigation>
-      <HomePage></HomePage>
+      {/* <HomePage></HomePage> */}
+      <ProductDetails/>
+      
       <Footer/>
     </div>
   );
---
+++ e-commerce/src/customer/components/ProductDetails/ProductReviewCard.jsx
@@ -0,0 +1,29 @@
+import { Avatar, Box, Grid, Rating } from '@mui/material'
+import React from 'react'
+
+const ProductReviewCard = () => {
+  return (
+    < Grid container spacing={2} gap={3}>
+        <Grid item xs={1}>
+            <Box>
+                <Avatar className='text-white sx={{width:56, height:56, bgcolor:"#9155fd"}}'>R</Avatar>
+            </Box>
+        </Grid>
+        
+        <Grid item xs={9}>
+            <div className='space-y-2'>
+                <div>
+                    <p className='font-semibold text-lg'>Manish</p>
+                    <p className='opacity-70'>April 5, 2023</p>
+                </div>
+            </div>
+            <Rating value={4.3} precision={.01} readOnly name='half-rating'/>
+            <p>Awesome fitting, superb color, I love this Shirt.</p>
+        </Grid>
+
+    </Grid>
+
+  )
+}
+
+export default ProductReviewCard
